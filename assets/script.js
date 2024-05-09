f98cc70012ca7f43772490449d7032ce62f50c2b
Rafi Nurfadly <rafinurfadly0@gmail.com>
2024-05-09 19:51:27 +0700

    add: news page, news detail and navbar

---
+++ assets/script.js
@@ -0,0 +1,97 @@
+const article = [
+  {
+    title:
+      "Joselu, Pemain Real Madrid yang Mengalami Transformasi Luar Biasa",
+    slug: "joselu-pemain-real-madrid-yang-mengalami-transformasi-luar-biasa",
+    content:
+      "Joselu menjadi tokoh utama dalam kemenangan dramatis Real Madrid melawan Bayern Munich di Semifinal Liga Champions. Dalam pertandingan itu, striker asal Spanyol ini mencetak dua gol yang membawa timnya melaju ke Final di Wembley. Namun, dua tahun yang lalu, situasinya sangat berbeda bagi pemain asal Spanyol ini, yang pada saat itu masih di luar radar Real Madrid.\n\nPada Final Liga Champions 2022, Real Madrid berhadapan dengan Liverpool di Stade de France. Joselu hadir dalam pertandingan itu, namun bukan sebagai pemain, melainkan sebagai penggemar setia. Dia bahkan mengunggah story di Instagram-nya untuk menyatakan kehadirannya di stadion tersebut.\n\nKala itu, Real Madrid berhasil mengalahkan Liverpool dengan skor 0-1, sementara Joselu berada di barisan klub lain, Alavés.\n\nDua tahun kemudian, Joselu menjadi pahlawan dengan mencetak dua gol yang membuat para pendukung Real Madrid berharap untuk meraih gelar Liga Champions ke-15 di Santiago Bernabéu.\n\nSetelah pertandingan, Joselu mengirim pesan kepada para pendukung Real Madrid di Meksiko. 'Saya ingin berterima kasih kepada seluruh penggemar Madrid di seluruh dunia. Terutama kepada Meksiko yang saya sukai, saya sudah beberapa kali pergi bersama keluarga saya dan mereka mendukung kami di Final.'\n\nMengenai pengalamannya mencetak dua gol yang membawa timnya lolos, Joselu menyebutnya sebagai sebuah mimpi yang menjadi kenyataan. 'Setiap orang bermimpi memiliki malam seperti ini. Saya selalu datang untuk membantu dan berkontribusi. Ancelotti tahu apa yang bisa saya berikan dan pada akhirnya saya senang dengan apa yang saya berikan. Senang karena memenangkan Liga dan berada di final Liga Champions,' ungkapnya.\n\nJoselu juga menyoroti peran mentalitas para pendukung, pemain, dan klub dalam kesuksesan Real Madrid. 'Suporter mendorong kami, dan itulah mentalitas Real Madrid. Itulah perasaan Real Madrid, mereka bisa bangkit. Pertandingan seperti ini juga harus dimenangkan dengan hati dan Madrid memiliki banyak hati,' katanya.\n\nMeskipun saat ini Joselu dipinjamkan oleh Espanyol, dia tidak menyembunyikan keinginannya untuk tetap bersama Real Madrid untuk musim depan. 'Saya fokus pada apa yang terjadi hari ini dan bahwa dalam tiga minggu ada final. Masa depan akan terlihat nanti. Saya ingin siap sepenuhnya untuk sisa musim,' tandasnya.",
+    category: "Sepakbola",
+    date: "2024-05-09",
+    time: "10:00",
+    image: "Artikel-1.jpg",
+  },
+  {
+    title: "Penundaan Grand Prix Kazakhstan MotoGP 2024",
+    slug: "penundaan-grand-prix-kazakhstan-motogp-2024",
+    content:
+      'MotoGP mengumumkan penundaan Grand Prix Kazakhstan. Diplot sebagai putaran kedelapan musim ini, balapan di Sokol International Racetrack diundur hingga batas waktu yang belum ditentukan.\n\nPenundaan terpaksa dilakukan menyusul bencana banjir yang melanda kawasan Asia Tengah. Seharusnya MotoGP Kazakhstan 2024 berlangsung pada 14-16 Juni mendatang setelah GP Italia di Mugello.\n\nKunjungan perdana MotoGP di Kazakhstan awalnya dijadwalkan tahun lalu sebagai bagian dari perluasan kalender kejuaraan dunia. Tetapi karena Sirkuit Sokol belum siap, balapan pun harus dibatalkan.\n\nGP Kazakhstan kembali dicantumkan dalam kalender balap musim ini, untuk akhir pekan 14-16 Juni 2024. Namun, pada 3 Mei, MotoGP menginformasikan bahwa race di Sokol mesti ditunda.\n\nMotoGP Kazakhstan 2024 ditangguhkan akibat situasi darurat banjir di wilayah Asia Tengah, yang meliputi sebagian besar Kazakhstan. Ini disebut bencana banjir terburuk di kawasan itu dalam 80 tahun terakhir.\n\nSungai Ural yang mengalir melalui Kazakhstan dan Rusia meluap akibat mencairnya salju dan es dengan cepat yang dipicu oleh suhu tinggi serta hujan lebat pada bulan April lalu.\n\n// Cuaca ekstrem yang belum pernah terjadi sebelumnya menyebabkan banjir di Asia Tengah, yang menyebabkan keadaan darurat nasional di Kazakhstan dan memaksa banyak penduduk untuk mengungsi,” kata pernyataan resmi MotoGP, Jumat (3/5/2024).\n\n// "MotoGP tidak bertanggung jawab untuk menambah beban kepada pihak berwenang atau layanan saat mereka berupaya membantu puluhan ribu orang yang terkena dampak di seluruh negeri.”\n\n// “Oleh karena itu, Sokol International Racetrack tidak akan menjadi tuan rumah MotoGP pada tanggal yang telah diumumkan sebelumnya, dan pembaruan lebih lanjut akan dipublikasikan setelah dikonfirmasi.”\n\n// “Sebagai prioritas utama kami, MotoGP menyampaikan belasungkawa dan dukungan kami kepada warga Kazakhstan dan semua yang terdampak banjir di Asia Tengah. Kami berharap negara, wilayah, dan seluruh komunitas dapat pulih secepatnya.”\n\nMeskipun belum ada tanggal baru untuk penyelenggaraannya, MotoGP memastikan bahwa balapan perdana di Kazakhstan tetap dilaksanakan tahun ini. Dan homologasi Sirkuit Sokol akan dilakukan setelah GP Catalunya, pada akhir Mei.',
+    category: "Olahraga",
+    date: "2024-05-09",
+    time: "12:00",
+    image: "Artikel-2.jpeg",
+  },
+  {
+    title:
+      "Harapan PBSI Usai Thomas-Uber Cup 2024, Menjelang Olimpiade 2024",
+    slug: "harapan-pbsi-usai-thomas-uber-cup-2024-menjelang-olimpiade-2024",
+    content:
+      "PBSI berharap para atletnya tetap menjaga semangat dan daya juang, serta berlatih lebih keras menjelang Olimpiade 2024, setelah hasil di Thomas-Uber 2024.\n\nSeperti yang diketahui, tim Thomas dan tim Uber Indonesia sama-sama menjadi runner-up dalam kejuaraan beregu putra dan putri tersebut.\n\nTim Uber Indonesia melebihi target semifinal dengan berhasil mencapai partai puncak. Sejak 2008, baru kali ini lagi tim putri Indonesia mampu mencapai final Uber Cup.\n\nSementara tim putra Indonesia gagal mencapai target juara. Namun, setidaknya tim Thomas Indonesia telah mampu mencapai final dalam tiga edisi terakhir Thomas Cup -- termasuk menjadi juara pada tahun 2020.\n\nHasil-hasil ini menjadi bekal bagi para atlet Merah Putih, terutama untuk persiapan menuju Olimpiade 2024 di Paris bulan Juli mendatang. Seperti yang diketahui, Thomas-Uber Cup menawarkan poin ranking yang besar, juga sebagai upaya untuk mengukur kekuatan Indonesia dengan pesaing-pesaing dari negara lain seperti Denmark, Taiwan, China, dan Korea.\n\nIndonesia telah berhasil meloloskan enam wakil ke Olimpiade 2024, yaitu tunggal putra Anthony Sinisuka Ginting dan Jonatan Christie, tunggal putri Gregoria Mariska Tunjung, ganda putra Fajar Alfian/Muhammad Rian Ardianto, ganda putri Apriyani Rahayu/Siti Fadia Silva Ramadhanti, dan ganda campuran Rinov Rivaldy/Pitha Haningtyas Mentari.\n\n'Mudah-mudahan ke depan, jika ada target besar untuk Olimpiade, tidak ada yang tidak mungkin,' kata Kabid Binpres PBSI Ricky Soebagdja kepada wartawan.\n\n'Dengan persiapan atau sisa waktu yang ada ini juga, persiapan untuk menuju Olimpiade ini lebih lagi semangat juangnya, latihan yang lebih keras lagi, ini yang kami harapkan,' ujarnya.\n\nJadi Bekal para Junior di Turnamen Besar\nPBSI tidak hanya mengirim pemain senior dalam Thomas dan Uber Cup, tapi juga ada pemain-pemain junior. Gabungan antara senior dan junior yang paling terlihat dalam sektor putri. Selain Apri/Fadia dan Gregoria, PBSI mengandalkan Ester Nurumi Tri Wardoyo, Komang Ayu Cahya Dewi, dan Ruzana.\n\nKemudian, ada ganda putri Rachel Allessya Rose/Meilysa Trias Puspitasari. Sedangkan di sektor putra, ada Alwi Farhan. Alwi yang akan berusia 19 tahun pada 12 Mei mendatang turun sebagai tunggal ketiga saat Indonesia mengalahkan Inggris dengan skor telak 5-0.\n\n'Ada catatan juga bagaimana di tim Thomas dan Uber di-mix. Artinya, pemain yang bisa dibilang senior dan junior, karena ada beberapa pemain yang baru juga ikut dalam tim, atau kejuaraan beregu Thomas atau Uber, tapi mereka sudah menunjukkan penampilan yang begitu luar biasa. Ini saya sekali lagi apresiasi yang setinggi-tingginya,' ucap Ricky.\n\n'Untuk itu, saya ucapkan sekali lagi terima kasih kepada para pemain, pendukung semua, juga kepada media, semua inilah yang kami capai di Tomas Cup dan Uber Cup 2024 ini. Tim Uber dan Tim Thomas memang sebagai pemenang nomor 2, tapi menurut saya mereka tetap juara karena perjuangan mereka yang begitu luar biasa,' kata Ricky.",
+    category: "Bulu Tangkis",
+    date: "2024-09-07",
+    time: "18:10",
+    image: "Artikel-3.jpeg",
+  },
+  {
+    title:
+      "Megawati Hangestri Kembali ke Red Sparks, Menanti Tandem Baru dan Mengembalikan Performa",
+    slug: "megawati-hangestri-kembali-ke-red-sparks-menanti-tandem-baru-dan-mengembalikan-performa",
+    content:
+      "Megawati Hangestri telah dipastikan akan kembali bermain untuk Red Sparks dalam Liga Bola Voli Putri Korea Selatan atau V League 2024-2025. Saat ini, Megawati masih memperkuat Jakarta BIN di Proliga 2024.\n\n1. Red Sparks\nMegawati akan bertahan lebih lama di kompetisi Liga Bola Voli Putri Korea Selatan atau V-League. Kepastian itu didapat setelah klub Daejeon JungKwanJang Red Sparks memperbarui kontraknya untuk satu musim. Red Sparks mengumumkan informasi tersebut pada Selasa, 30 April 2024.\n\nTim voli putri asal Daejeon ini puas dengan performa Megawati yang berkontribusi besar dalam keberhasilan Red Sparks finis ketiga pada musim ini. “Tanpa keraguan, kami memutuskan untuk memperbarui kontrak bersama Mega,” kata kepala pelatih Red Sparks, Ko Hee-jin, dikutip dari The Sports Times.\n\n2. Menanti Tandem Baru\nGiovanna Milana, yang musim lalu menjadi rekannya, tak masuk dalam daftar pemain seleksi kali ini. Pemain asal Amerika itu kini bermain di Proliga Indonesia, bersama Jakarta Pertamina Enduro. Empat pemain lama yang kembali mengikuti try out adalah Letizia Moma (Kamerun), Willow Johnson (Amerika Serikat), Gisele Silva (Kuba-Azerbaijan), dan Vanja Bukiric (Serbia). Musim lalu, Moma membawa Hillstate juara dan terpilih menjadi pemain terbaik atau MVP final melawan Pink Spiders.\n\nTes pemain asing itu dilakukan Federasi Bola Voli Korea (KOVO) di Dubai, Uni Emirat Arab pada Selasa, 7 Mei, hingga Kamis, 9 Mei.\n\n3. Giovanna Milana\nDuel antara Megawati dan Giovanna Milana gagal. Giovanna Milana cedera yang mengharuskannya mundur dari Proliga. 'Ini bukan cedera yang serius, tetapi akan memakan waktu beberapa pekan untuk saya kembali berolahraga, yang berarti saya akan melewatkan sekitar 8-10 pertandingan (hampir sepanjang musim). Agar Pertamina memiliki kesempatan untuk menjalani musim yang sukses, saya harus diganti saat cedera... Setelah itu, saya tidak bisa kembali sesuai aturan liga,' kata Giovanna Milana melalui postingan Instagramnya @gia__day, Selasa, 7 Mei 2024.\n\n4. Menanti Performa Megawati\nPada Senin, 30 April 2024, Red Sparks mengumumkan, Megawati Hangestri akan bertahan lebih lama bersama klub mereka untuk tampil di Liga Voli Putri Korea Selatan atau V League 2024-2025.. “Megawati pun ingin menunjukkan performa lebih baik pada musim depan. Kami akan mempersiapkan diri dengan baik demi menciptakan sinergi bagus sejak awal musim,” kata pelatih Red Sparks Ko Hee Jin.\n\n5. Dua Kali\nDari tiga laga yang dijalani Jakarta BIN, Megawati baru tampil dua kali. Dalam kedua laga saat ia tampil, Jakarta BIN juga terus kalah. Dalam kedua laga ini, Megawati menyumbang 39 poin. Proliga 2024 pekan ketiga akan berlangsung di Palembang. Megawati akan kembali tampil pada Kamis, 9 Mei 2024, saat Jakarta BIN melawan Jakarta Elektrik PLN.",
+    category: "Bola Voli",
+    date: "2024-05-08",
+    time: "16:41",
+    image: "Artikel-4.jpeg",
+  },
+  {
+    title: "Nikola Jokic Raih MVP Ketiga dalama empat musim",
+    slug: "nikola-jokic-raih-mvp-ketiga-dalama-empat-musim",
+    content:
+      "Bintang Denver Nuggets Nikola Jokic dinobatkan sebagai MVP NBA untuk ketiga kalinya dalam empat musim pada hari Rabu (8/5) waktu Amerika Serikat, yang semakin mengukuhkan statusnya di antara pemain bola basket terhebat sepanjang masa. Jokic menjadi pemain kesembilan dalam sejarah liga dan pemain internasional pertama yang memenangkan penghargaan tersebut setidaknya tiga kali.\n\nNBA mengumumkan Jokic sebagai pemenang atas garda Oklahoma City Thunder Shai Gilgeous-Alexander dan garda Dallas Mavericks Luka Doncic, yang keduanya merupakan finalis. Jokic memperoleh 79 dari 99 suara peringkat pertama, mengungguli finalis Gilgeous-Alexander (15 suara) dan Doncic (4 suara). Forwarda Milwaukee Bucks Giannis Antetokounmpo, yang menerima satu suara tempat pertama, dan garda New York Knicks Jalen Brunson masing-masing menempati posisi keempat dan kelima.\n\n“Anda harus memulai dengan rekan satu tim Anda,” kata Jokic dalam sebuah wawancara dengan TNT . “Tanpa mereka, saya tidak bisa berbuat apa-apa. Pelatih, pemain, organisasi, staf medis, pelatih kekuatan, pelatih pengembangan, semuanya merupakan satu lingkaran besar. Saya tidak bisa menjadi apa pun tanpa mereka.”\n\nKomentator TNT Shaquille O'Neal kemudian mengatakan kepada Jokic bahwa dia yakin Gilgeous-Alexander pantas memenangkan penghargaan tahun ini.\n\n“Terima kasih, Shaq,” kata Jokic. “Kami tidak menghakimi orang di sini. Tidak apa-apa. Itu adalah pendapat Anda. Saya bercanda. Sebenarnya, ada banyak pemain yang pantas mendapatkannya.”\n\nJokic, mencetak rata-rata 26,4 poin, 12,4 rebound, dan 9,0 asis dalam 79 pertandingan, memimpin Nuggets meraih 57 kemenangan dan unggulan kedua Wilayah Barat. Satu-satunya pemain dalam sejarah liga yang menyamai rata-rata tersebut adalah guard Hall of Fame Oscar Robertson pada tahun 1962 .\n\nJokic menjadi pemain ke-9 dalam sejarah liga yang memenangkan tiga atau lebih penghargaan MVP. Bergabung dengan pemain elit NBA seperti Kareem Abdul-Jabbar (enam), Bill Russell (lima), Michael Jordan (lima), Wilt Chamberlain (empat), LeBron James (empat), Moses Malone (tiga), Larry Bird (tiga) dan Magic Johnson (tiga) adalah satu-satunya pemain lain yang memenangkan penghargaan setidaknya tiga kali. NBA menamai penghargaan MVP-nya untuk menghormati Jordan pada tahun 2022.\n\nJokic memimpin Nuggets dalam poin, rebound, dan asis dan finis di 10 besar NBA di setiap kategori musim ini. Jokic juga memimpin liga dalam peringkat efisiensi pemain, sebuah ukuran yang memperkirakan produktivitas per menit pemain dengan mengukur semua pencapaian statistiknya, positif dan negatif. Jokic telah memimpin liga di PER selama empat musim berturut-turut dan kini menjadi pemimpin NBA sepanjang masa dalam kategori tersebut. Michael Jordan dan LeBron James berada di urutan kedua dan ketiga dalam daftar sepanjang masa. (*)",
+    category: "Bola Basket",
+    date: "2024-05-09",
+    time: "10:21",
+    image: "Artikel-5.jpeg",
+  },
+]
+
+const newsList = document.querySelector(".news-list")
+
+article.forEach((item) => {
+  const cardSection = document.createElement("section")
+  cardSection.classList.add("card")
+
+  const cardImage = document.createElement("img")
+  cardImage.src = `assets/img/${item.image}`
+  cardImage.alt = `${item.title} Image`
+  cardImage.quality = 95
+  cardImage.classList.add("card-image")
+
+  const cardInfo = document.createElement("div")
+  cardInfo.classList.add("card-info")
+
+  const cardTitle = document.createElement("a")
+  cardTitle.href = `/${item.slug}.html`
+  cardTitle.classList.add("card-title")
+  cardTitle.textContent = item.title
+
+  const badgesDiv = document.createElement("div")
+  badgesDiv.classList.add("badges")
+
+  const categoryBadge = document.createElement("span")
+  categoryBadge.classList.add("category-badge")
+  categoryBadge.textContent = item.category
+
+  const cardDate = document.createElement("p")
+  cardDate.classList.add("card-date")
+  cardDate.textContent = `Admin | ${item.date} | ${item.time} WIB`
+
+  badgesDiv.appendChild(categoryBadge)
+  cardInfo.appendChild(cardTitle)
+  cardInfo.appendChild(badgesDiv)
+  cardInfo.appendChild(cardDate)
+  cardSection.appendChild(cardImage)
+  cardSection.appendChild(cardInfo)
+
+  // Append the card section to the news list
+  newsList.appendChild(cardSection)
+})
